23843b356d38943edea20f9c3e50b4bdb102309a
edrick <edrickepascual@gmail.com>
2016-08-06 12:13:30 -0700

    Updated Color scheme

---
+++ FirebaseSocialApp/FirebaseSocialApp/CustomView.swift
@@ -0,0 +1,15 @@
+//
+//  CustomView.swift
+//  FirebaseSocialApp
+//
+//  Created by Edrick Pascual on 8/6/16.
+//  Copyright © 2016 Edge Designs. All rights reserved.
+//
+
+import UIKit
+
+class CustomView: UIView {
+
+  
+
+}
