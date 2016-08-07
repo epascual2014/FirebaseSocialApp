6a9a92a1afaea01e2bf900987fad0fb0c5fb268b
edrick <edrickepascual@gmail.com>
2016-08-06 17:05:24 -0700

    Email Auth Completed

--- FirebaseSocialApp/FirebaseSocialApp/LoginViewController.swift
+++ FirebaseSocialApp/FirebaseSocialApp/LoginViewController.swift
@@ -15,8 +15,10 @@ class LoginViewController: UIViewController {
 
     @IBOutlet weak var facebookButton: RoundButton!
     
+    @IBOutlet weak var emailTextfield: CustomTextfield!
+    @IBOutlet weak var passwordTextfield: CustomTextfield!
+    
     @IBAction func facebookActionTapped(_ sender: AnyObject) {
-        
         
         // Facebook Authentication
         let facebookLogin = FBSDKLoginManager()
@@ -46,6 +48,23 @@ class LoginViewController: UIViewController {
         })
     }
 
+    @IBAction func signinButtonTapped(_ sender: AnyObject) {
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            FIRAuth.auth()?.signIn(withEmail: email, password: password, completion: { (user, error) in
+                if error == nil {
+                    print("ED: Email user auth with Firebase")
+                } else {
+                    FIRAuth.auth()?.createUser(withEmail: email, password: password, completion: { (user, error) in
+                        if error != nil {
+                            print("ED: Unable to auth Firebase w/ Email")
+                        } else {
+                            print("ED: Successfull auth with Firebase")
+                        }
+                    })
+                }
+            })
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
